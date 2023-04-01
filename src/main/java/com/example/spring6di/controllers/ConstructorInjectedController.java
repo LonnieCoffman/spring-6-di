f3ac325c804367e451d5c06527bd26ac1e41cf43
Lonnie Coffman <lonniecoffman@icloud.com>
2023-04-01 08:30:35 -0500

    di methods within spring

--- src/main/java/com/example/spring6di/controllers/ConstructorInjectedController.java
+++ src/main/java/com/example/spring6di/controllers/ConstructorInjectedController.java
@@ -1,7 +1,9 @@
 package com.example.spring6di.controllers;
 
 import com.example.spring6di.services.GreetingService;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
     private final GreetingService greetingService;
 
